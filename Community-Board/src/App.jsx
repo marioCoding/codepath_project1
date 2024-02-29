3264008aa24cff56caee76931534570f1154a9f9
marioCoding <marosw.sal@gmail.com>
2024-02-28 16:25:18 -0800

    started the board component

---
+++ Community-Board/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+
+const App = () => {
+
+  return (
+    <div className="App">
+      <h1>Calender</h1>
+      <h2>Here it is</h2>
+      
+    </div>
+  )
+}
+
+export default App
